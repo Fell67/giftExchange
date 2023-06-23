1821b60a073f8751386ef463ce3786e1b0d12817
Victoria Abreu <101905935+Fell67@users.noreply.github.com>
2023-06-23 09:41:50 -0600

    Add dark theme
    * Added the colors for the dark theme and a toggle button that changes between Dark and Light themes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,7 @@ import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 
 // My themes
-const customLightTheme = {
+const Light = {
     dark: false,
     colors: {
         background: "#FFFFFF", // white
@@ -14,13 +14,24 @@ const customLightTheme = {
     }
 }
 
+const Dark = {
+    dark: true,
+    colors: {
+        background: "#000000", // black
+        primary: "#212121", // dark grey
+        secondary: "#023047", // dark blue
+        accent: "#FFB703" // golden yellow
+    }
+}
+
 const vuetify = createVuetify({
     components,
     directives,
     theme: {
-        defaultTheme: "customLightTheme",
+        defaultTheme: "Light",
         themes: {
-            customLightTheme,
+            Light,
+            Dark
         }
     },
 })
