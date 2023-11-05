0663c2b1529c0c9fff9e3904de1a268d76c0d735
Fell67 <ugotvictoria64@gmail.com>
2023-11-04 21:42:12 -0400

    full coverage on PeopleExchangingGifts.vue file

---
+++ src/unit-tests/components/PeopleExchangingGifts.test.js
@@ -0,0 +1,37 @@
+import { mount } from "@vue/test-utils"
+import { describe, expect, it, vi } from "vitest"
+import { createTestingPinia } from "@pinia/testing"
+
+import PeopleExchangingGifts from "@/components/content/PeopleExchangingGifts.vue"
+
+function createWrapper () {
+    return mount(PeopleExchangingGifts, {
+        plugins: [
+            createTestingPinia({
+                createSpy: vi.fn,
+                stubActions: true
+            })
+        ]
+    })
+}
+
+describe("Testing the PeopleExchangingGifts component", () => {
+    // Testing that when the continue button is clicked the component emits a goToDifferentPage event with "continue"
+    it("Testing that when the continue button is clicked the component emits a goToDifferentPage event with 'continue'", () => {
+        let wrapper = createWrapper()
+        wrapper.find("#continue-button").trigger("click")
+        expect(wrapper.emitted("goToDifferentPage")[0]).toEqual(["continue"])
+    }),
+    // Testing that when the go back button is clicked the component emits a goToDifferentPage event with "goBack"
+    it("Testing that when the go back button is clicked the component emits a goToDifferentPage event with 'goBack'", () => {
+        let wrapper = createWrapper()
+        wrapper.find("#go-back-button").trigger("click")
+        expect(wrapper.emitted("goToDifferentPage")[0]).toEqual(["goBack"])
+    }),
+    // Testing that when goToDifferentPage gets an invalid direction that it returns an error
+    it("Testing that when the go back button is clicked the component emits a goToDifferentPage event with 'goBack'", () => {
+        let wrapper = createWrapper()
+        let response = wrapper.vm.goToDifferentPage("badDirection")
+        expect(response).toContain("Error")
+    })
+})
--- src/unit-tests/components/Welcome.test.js
+++ src/unit-tests/components/Welcome.test.js
@@ -7,6 +7,9 @@ import { mangle } from "marked-mangle"
 import { gfmHeadingId } from "marked-gfm-heading-id"
 
 const readMe = new URL("README.md", "https://fell67.github.io/giftExchange/")
+function createWrapper () {
+    return mount(Welcome, {})
+}
 const wrapper = mount(Welcome, {})
 
 // Create the html that would be made from the readMe file to compare with the function
@@ -32,21 +35,21 @@ async function createTestReadMeHTML () {
 describe("Testing the Welcome component", () => {
     // Test that the readme can load correctly
     it("Test that the readme can load correctly", async () => {
+        let wrapper = createWrapper()
         let readMeFromTest = await createTestReadMeHTML()
         let readMeFromMethod = await wrapper.vm.markdownToHtml(readMe)
         expect(readMeFromMethod).to.equal(readMeFromTest)
     }),
     // Test that the markdownToHtml throws an error when given a bad url
     it("Test that the markdownToHtml throws an error when given a bad url", async () => {
+        let wrapper = createWrapper()
         let readMeFromMethod = await wrapper.vm.markdownToHtml(new URL("badurl.md", "https://fell67.github.io/giftExchange/"))
         expect(readMeFromMethod).toContain("Error")
     }),
     // Testing that when the continue button is clicked the component emits a goToDifferentPage event with "continue"
     it("Testing that when the continue button is clicked the component emits a goToDifferentPage event with 'continue'", () => {
+        let wrapper = createWrapper()
         wrapper.find("#continue-button").trigger("click")
-        wrapper.vm.$nextTick(() => {
-            wrapper.vm.goToDifferentPage()
-            expect(wrapper.emitted().goToDifferentPage[0]).toEqual(["continue"])
-        })
+        expect(wrapper.emitted('goToDifferentPage')[0]).toEqual(["continue"])
     })
 })
