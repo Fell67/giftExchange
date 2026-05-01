19c639951cb4d2ce968b8aa8cb1ce37b971b523f
Fell67 <ugotvictoria64@gmail.com>
2026-05-01 16:06:54 +0000

    Deploy

--- src/class/personClass.js
+++
@@ -1,8 +0,0 @@
-export class Person {
-    constructor (name) {
-        this.name = name, // This is the current name of the person 
-        this.edit = false, // This keeps track on if the user is editing the name
-        this.editName = name, // This is the name the user is editing the person's name to
-        this.nameDrawn = '' // This is the name of who they have drawn
-    }
-}
